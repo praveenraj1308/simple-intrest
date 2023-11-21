fba177b8b662326058b93a3105cc1d75a79a9b74
praveenraj1308 <110396123+praveenraj1308@users.noreply.github.com>
2023-11-21 14:15:56 +0530

    Create simple intrest.py

---
+++ simple intrest.py
@@ -0,0 +1,27 @@
+Leena wanted to start a business and she was looking for a venture capitalist. Through her frend, she met a construction company owner Meena,
+who was interested in investing in an emerging business. Looking at the business proposal, the owner was very much impressed with Leena's work.
+So she decided to invest in Leena's business and hence gave a green signal to go ahead with the project. Leena got a loan of Rs.
+X for a period of Y years from the owner at Rs interest per annum. Find the rate of interest and the total amount to be returned by Leerna tothe owner.
+The owner was impressed by the proper repayment of the financed amount and decides to give a special offer of a 2% discount on the total Interest at the end of the settlement.
+Find the discount amount and also find the totalamount given back by LeenaNote: All rupee values should be in two decimal points.
+
+Sample input 1
+100
+1
+18
+Sample Output 1
+10.00
+110.00
+0.20
+109.80
+
+
+
+p=float(input())
+n=float(input())
+r=float(input())
+I=(p*n*r)/100
+A=I=p
+D=I*0.02
+T=A-D
+print(I,a,D,T)
